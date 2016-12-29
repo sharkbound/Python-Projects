29315402622217ad1a7971e6e97ac09e2f5d939b
sharkbound <laptopblaster@gmail.com>
2016-12-29 14:33:13 -0800

    added setup file

---
+++ import_test/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='import_test',
+    version='1.1',
+    packages=[''],
+    package_dir={'': 'deps'},
+    url='',
+    license='',
+    author='owner',
+    author_email='',
+    description=''
+)
