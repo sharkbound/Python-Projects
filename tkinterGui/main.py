94259649dd23e29b1c4725a2425c3c543931cab6
sharkbound <laptopblaster@gmail.com>
2016-12-31 18:01:21 -0800

    ...

--- tkinterGui/main.py
+++ tkinterGui/main.py
@@ -7,11 +7,16 @@ gui = gui_util
 
 def main():
     t1 = time.clock()
+    printmemory()
 
+    gui.set_width_height(200, 200)
+    gui.addbtn("button name")
     gui.set_title("Test title")
-    gui.packtxt("testing test")
+    gui.addtxt("testing test")
+    gui.addchkbtn("checkbutton text")
     gui.mainloop()
 
+    printmemory()
     t2 = time.clock()
     print(f'Execution time: \n\t{t2-t1}')
 
--- tkinterGui/src/GUI/gui_util.py
+++ tkinterGui/src/GUI/gui_util.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter
 
 gui = Tk()
 
@@ -8,10 +9,17 @@ def addmenuitem(item):
     pass
 
 
-def packtxt(text):
+def addbtn(text, *func):
+    button = tkinter.Button(gui, text="textbtn")
+    button.pack()
+
+
+def addtxt(text):
     label = Label(gui, text=text)
     label.pack()
-
+def addchkbtn(text):
+    chkbtn = Checkbutton(gui, text=text)
+    chkbtn.pack()
 
 def startgui():
     gui.mainloop()
@@ -19,3 +27,8 @@ def startgui():
 
 def set_title(title):
     gui.title(title)
+
+
+def set_width_height(width, height):
+    frame = Frame(gui, width=width, height=height)
+    frame.pack()
