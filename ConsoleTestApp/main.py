f6592de690d17702e6506e07c7315e2a1084961b
sharkbound <laptopblaster@gmail.com>
2017-01-08 14:57:34 -0800

    ...

--- ConsoleTestApp/main.py
+++ ConsoleTestApp/main.py
@@ -1,9 +1,9 @@
 from src.timing import Timer
 from colorama import Fore, Back
 from functools import lru_cache
+from collections import namedtuple
 
-import math, bettertimeit
-import timeit, time
+import timeit, time, struct
 
 
 @lru_cache(1000)
@@ -13,10 +13,6 @@ def factorial(n):
     return n * factorial(n - 1)
 
 
-def fact():
-    print(f'Factorial of 5 {factorial(5)}')
-
-
 def main():
     f = 5
     print(f'The factorial of {f} is {factorial(f)}')
@@ -24,19 +20,18 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # t = Timer()
-    # t.start_time()
-    # time.sleep(5)
-    # t.print()
-    #
-    # t2 = Timer()
-    # t2.time_exec(lambda: (
-    #     print(1),
-    #     time.sleep(3),
-    #     print(2)
-    # ), 2)
 
-    # t2.time_exec(lambda: time.sleep(2), 2)
-    # print(f'{Fore.GREEN}GREEN{Fore.RED}RED{Fore.LIGHTYELLOW_EX}YELLOW')
-    # value = timeit.timeit(stmt=main, number=1)
-    # print(f'value: {value}')
+    timer = Timer()
+    time.sleep(5)
+    print(f'{Fore.BLUE}The time after sleeping is: {timer.get_time()}')
+    timer.reset()
+    time.sleep(3)
+    print(f'Time after 3 seconds of sleep: {timer.get_time()}')
+    print(f'{Fore.LIGHTRED_EX}Total time: {timer.get_total_time()}')
+
+# equivilant of C# structs in python:
+
+# structtest = namedtuple('structtest', 'f1, f2, f3, f4')
+# st = structtest('var1', 'var2', 'var3', 'var4')
+# print(Fore.CYAN+st[2])   both work.
+# print(Fore.CYAN+st.f3)
--- ConsoleTestApp/src/timing/__init__.py
+++ ConsoleTestApp/src/timing/__init__.py
@@ -4,37 +4,26 @@ import time
 class Timer:
     def __init__(self):
         self.start = time.time()
-        self.end = time.time()
         self.const_start = time.time()
 
-    def start_time(self):
-        self.start = time.time()
-
-    def reset_all(self):
-        self.start = time.time()
-        self.end = time.time()
-
-    def reset_end(self):
-        self.end = time.time()
-
-    def reset_start(self):
+    def reset(self):
         self.start = time.time()
 
     def print_reset(self):
-        self.end = time.time()
-        print(f'Execution time: {self.end-self.start}')
-        self.reset_all()
+        print(f'Execution time: {time.time()-self.start}')
+        self.reset()
 
     def print(self):
-        self.end = time.time()
-        result = self.end - self.start
-        print(f'Execution time: {self.end-self.start}')
+        print(f'Execution time: {time.time()-self.start}')
 
     def get_time(self):
-        return self.end - self.start
+        return time.time() - self.start
+
+    def get_total_time(self):
+        return time.time() - self.const_start
 
     def time_exec(self, func, times=1):
-        self.reset_all()
+        self.reset()
         for x in range(times):
             func()
         self.print_reset()
