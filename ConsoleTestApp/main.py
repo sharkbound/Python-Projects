668bd03623449998a108bc8cd58ad4c4a3fac618
sharkbound <laptopblaster@gmail.com>
2017-01-09 13:17:10 -0800

    changes

--- ConsoleTestApp/main.py
+++ ConsoleTestApp/main.py
@@ -2,6 +2,7 @@ from src.timing import Timer
 from colorama import Fore, Back
 from functools import lru_cache
 from collections import namedtuple
+from src.logger import *
 
 import timeit, time, struct
 
@@ -20,18 +21,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    timer = Timer()
-    time.sleep(5)
-    print(f'{Fore.BLUE}The time after sleeping is: {timer.get_time()}')
-    timer.reset()
-    time.sleep(3)
-    print(f'Time after 3 seconds of sleep: {timer.get_time()}')
-    print(f'{Fore.LIGHTRED_EX}Total time: {timer.get_total_time()}')
-
-# equivilant of C# structs in python:
-
-# structtest = namedtuple('structtest', 'f1, f2, f3, f4')
-# st = structtest('var1', 'var2', 'var3', 'var4')
-# print(Fore.CYAN+st[2])   both work.
-# print(Fore.CYAN+st.f3)
