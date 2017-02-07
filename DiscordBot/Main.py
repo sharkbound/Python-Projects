7ae7d620fa5691bcc1e45a705590ab482026063d
sharkbound <laptopblaster@gmail.com>
2017-02-06 19:51:02 -0800

    got base command working on discord bot

--- DiscordBot/Main.py
+++ DiscordBot/Main.py
@@ -4,16 +4,32 @@ import sys
 import os
 
 from Util.config import BotConfig
-from Bot.DiscordBot import Bot
+from discord.ext.commands import Bot
 
-# sys.path.append(".\\Bot")
-# sys.path.append(".\\Util")
+sys.path.append(".\\Bot")
+sys.path.append(".\\Util")
 
 
 def main():
     cfg = BotConfig()
-    bot = Bot()
+    bot = Bot(command_prefix=cfg.command_prefix)
 
+    @bot.event
+    @asyncio.coroutine
+    def on_read():
+        print('client logged in...')
+
+
+    @bot.command()
+    async def help1(*args):
+        return await bot.say('Default help text')
+
+    @bot.command()
+    async def quit(*args):
+        await bot.say("**__Swims deep into the ocean...__**")
+        return await bot.logout()
+
+    bot.run(cfg.token)
 
 if __name__ == '__main__':
     main()
--- DiscordBot/Util/config.py
+++ DiscordBot/Util/config.py
@@ -13,6 +13,13 @@ class BotConfig:
         self.config_folder_name = "Config"
         self._create_default_config()
 
+        self.config_data = self.get_config_values()
+        self.connection_info = self.config_data['ConnectionInfo']
+        self.settings = self.config_data['Settings']
+        self.command_prefix = self.settings['commandprefix']
+        self.token = self.connection_info['Token']
+        self.owner_id = self.settings['OwnerID']
+
     def _create_default_config(self):
         json_str = json.dumps(self._get_default_settings(), indent="\t")
 
@@ -29,5 +36,21 @@ class BotConfig:
             'OwnerID': 'change me',
             'Token': 1111111111111
         }
+        default['Settings'] = {
+            "commandprefix": "<",
+            "placeholder": "<"
+        }
 
         return default
+
+    def get_config_values(self):
+        data = ""
+        read_list = []
+
+        with open(self.config_path_complete) as file:
+            read_list = file.readlines()
+
+        for string in read_list:
+            data += string
+
+        return json.loads(data)
