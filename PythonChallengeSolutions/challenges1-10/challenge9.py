8db0127b5956175645a291a41c5e77818c0f373b
sharkbound <laptopblaster@gmail.com>
2017-04-01 16:30:08 -0700

    added a encrytion module i am working on

--- PythonChallengeSolutions/challenges1-10/challenge9.py
+++ PythonChallengeSolutions/challenges1-10/challenge9.py
@@ -1,16 +1,28 @@
 import re
 import requests
+import sys
+from PIL import Image, ImageDraw
+
+edited_img_path = '../files/good_edited.jpg'
+base_img_path = '../files/good.jpg'
 
 # user: huge
 # pass: file
 if __name__ == '__main__':
-    url = 'http://www.pythonchallenge.com/pc/return/good.html:huge:file'
-    text = ''
     with open('../files/challenge9.html') as f:
         text = f.read().replace('\n', '')
 
-    first_text = re.findall('first:\s*[0-9,]*', text)
-    second_text = re.findall('second:\s*[0-9,]*', text)
+    text_list = re.findall('first:\s*[0-9,]*|second:\s*[0-9,]*', text)
 
-    print(first_text)
-    print(second_text)
+    first = [int(i) for i in re.findall('([0-9]{3})', text_list[0])]
+    second =  [int(i) for i in re.findall('([0-9]{3})', text_list[1])]
+    merged = first+second
+
+    new_image = Image.new('RGB', (640, 840))
+
+    for i in range(0, len(merged)-1, 2):
+        x,y = merged[i], merged[i+1]
+        new_image.putpixel((x,y), (255,255,255))
+
+    new_image.save(edited_img_path)
+
---
+++ PythonFiles/UsefulPythonScripts/encryption.py
@@ -0,0 +1,57 @@
+import base64
+
+from string import ascii_letters
+
+
+def encode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = "".join(encoded_chars)
+    return encoded_string  # base64.urlsafe_b64encode(encoded_string)
+
+
+def decode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) - ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = "".join(encoded_chars)
+    return encoded_string  # base64.urlsafe_b64encode(encoded_string)
+
+
+def encrypt(string, step=10):
+    enc_str = ''
+    for i, c in enumerate(string):
+        new_char = chr(ord(c) + i + step)
+        enc_str += new_char
+    return enc_str
+
+
+def decrypt(string, step=10):
+    enc_str = ''
+    for i, c in enumerate(string):
+        new_char = chr(ord(c) - i - step)
+        enc_str += new_char
+    return enc_str
+
+
+def encrypt_key(string, key, step=0):
+    key = key * (len(string) - 1)
+    enc_str = ''
+    for i, c in enumerate(string):
+        new_char = chr(ord(c) + ord(key[i]) + step)
+        enc_str += new_char
+    return enc_str
+
+
+def decrypt_key(string, key, step=0):
+    key = key * (len(string) - 1)
+    enc_str = ''
+    for i, c in enumerate(string):
+        new_char = chr(ord(c) - ord(key[i]) - step)
+        enc_str += new_char
+    return enc_str
