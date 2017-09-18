efc3ad67541995c77c7aac9e673604c739f3b2e0
sharkbound <laptopblaster@gmail.com>
2017-09-18 12:57:35 -0700

    working on a custom config parser system

--- SharkFig/helper.py
+++ SharkFig/helper.py
@@ -5,8 +5,3 @@ def read_file_lines(fname):
 def write_to_file(fname, text=''):
     with open(fname, 'w') as write_file:
         write_file.write(text)
-
-def dict_test(**kwargs):
-    a = kwargs.get('A', 'a')
-    b = kwargs.get('B', 'b')
-    return a + b
--- SharkFig/parser.py
+++ SharkFig/parser.py
@@ -1,3 +1,27 @@
-import helper
+import warnings
 
-print(*helper.read_file_lines('parser.py'))
+import helper
+import re
+
+GET_KEY_VALUE_RE = re.compile(r'(\w+)\s?=\s?(.+)')
+ASSIGNMENT_RE = re.compile(r'.+=.+')
+STR_ASSIGNMENT_RE = re.compile(r'(\w+)\s?=\s?"(.+)"')
+
+def parse(fname) -> dict:
+    dct = {}
+    for l in helper.read_file_lines(fname):
+        if ASSIGNMENT_RE.match(l):
+            key,value = map(str.strip, GET_KEY_VALUE_RE.findall(l)[0])
+
+            iv = parse_to_int(value)
+            if iv[0]:
+                dct[key] = iv[1]
+            elif '"' in value:
+                dct[key] = value.strip('"')
+    return dct
+
+def parse_to_int(value):
+    try:
+        return (True, int(value))
+    except:
+        return (False, None)
