caae577cc28d99654d043d53bff16636dc70bdd4
sharkbound <laptopblaster@gmail.com>
2020-06-07 15:05:51 -0500

    mcd backup script

--- util_scripts/backup_mcd.py
+++ util_scripts/backup_mcd.py
@@ -7,8 +7,5 @@ GAME_SAVE = Path(os.getenv('APPDATA'), '../local/dungeons').absolute()
 WORKING = Path().absolute()
 NOW = datetime.now()
 BACKUP = Path('backups/').absolute() / f'{NOW.year}_{NOW.month}_{NOW.day}__{NOW.hour}_{NOW.minute}'
-#
-# if not BACKUP.exists():
-#     BACKUP.mkdir(parents=True)
 
 shutil.copytree(GAME_SAVE, BACKUP)
