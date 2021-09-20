404bd02b7eedeff4431fd8d8136c21f574e6ab80
sharkbound <laptopblaster@gmail.com>
2021-09-20 17:08:40 -0500

    dotdict is now improved to convert data on request

--- utils/dotdict.py
+++ utils/dotdict.py
@@ -1,32 +1,48 @@
-import json
-from collections import OrderedDict
+import typing
 
 
-class DotDict(OrderedDict):
-    _missing: 'DotDict' = None
-
+class DotDict(dict):
     def __getattr__(self, item):
-        return self[item]
+        return self.__getitem__(item)
 
     def __setattr__(self, key, value):
         self[key] = value
 
+    def _is_mutable_mapping(self, mapping):
+        return isinstance(mapping, typing.MutableMapping) and type(mapping) is not type(self)
+
+    def __getitem__(self, item):
+        value = super().__getitem__(item)
+
+        if self._is_mutable_mapping(value):
+            value = DotDict(value)
+            self[item] = value
+
+            for key, subvalue in tuple(value.items()):
+                if self._is_mutable_mapping(subvalue):
+                    value[key] = type(self)(subvalue)
+
+        elif isinstance(value, typing.MutableSequence):
+            for index, item in enumerate(value):
+                if self._is_mutable_mapping(item):
+                    value[index] = DotDict(item)
+
+        return value
+
     def __missing__(self, key):
-        if self.__class__._missing is None:
-            self.__class__._missing = DotDict()
-        return self.__class__._missing
+        self[key] = self.__class__()
+        return self[key]
 
-    @property
-    def json(self):
-        return json.dumps(self)
+    def __repr__(self):
+        return f'{self.__class__.__name__}({super().__repr__()})'
 
 
-# testing code
 if __name__ == '__main__':
-    d = DotDict()
-    jimmy = d.jimmy
-    jimmy.age, jimmy.name = 1, 'jimmy'
-    d['jimmy']['friends'] = [1, 2, 3]
-
-    print(d)
-    print(d.json)
+    dot = DotDict()
+    dot.value = {'subvalue': 'value'}
+    dot.list = [{'test': 'value'}]
+    print(dot)
+    print(dot.value)
+    print(dot)
+    print(dot.list)
+    print(dot)
